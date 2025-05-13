079e3ea64424b1b5f304e1bbf0910c540ed63299
apetitco <apetitco@student.42.fr>
2025-05-13 13:15:37 +0200

    Refined ex05

--- ex05/src/main.cpp
+++ ex05/src/main.cpp
@@ -7,9 +7,16 @@ int	main()
 	std::string	s2 = "INFO";
 	std::string	s3 = "WARNING";
 	std::string	s4 = "ERROR";
+	std::string	s5 = "BANANE";
+	std::string	s6 = "0";
+	std::string	s7 = "";
 
 	harl.complain(s1);
 	harl.complain(s2);
 	harl.complain(s3);
 	harl.complain(s4);
+	std::cout << "Tests (shouldn't display anything below, except destructor call):" << std::endl;
+	harl.complain(s5);
+	harl.complain(s6);
+	harl.complain(s7);
 }
